c2f6dbb545d3610c9df31d367d2df2c5033ac982
alisselisss <talisa220903@mail.ru>
2024-06-28 13:48:20 +0300

    added installation of images from Docker Hub

--- src/docker_image_loader.py
+++ src/docker_image_loader.py
@@ -19,20 +19,33 @@ def image_exists(image):
         return False
 
 
+def pull_docker_image(image):
+    try:
+        subprocess.run(["docker", "pull", image], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+
 def save_docker_image(image, directory):
     if not image_exists(image):
-        return False
+        print(f"Image {image} not found locally. Attempting to pull from Docker Hub...")
+        if not pull_docker_image(image):
+            print(f"Failed to pull image {image} from Docker Hub.")
+            return False
 
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    file_name = os.path.join(directory, image.replace(":", "_") + ".tar")
+    image_name = image.replace("/", "_").replace(":", "_")
+    file_name = os.path.join(directory, image_name + ".tar")
+
     try:
         subprocess.run(["docker", "save", "-o", file_name, image], check=True)
         return True
     except subprocess.CalledProcessError as e:
-        pass
-    return False
+        print(f"Failed to save Docker image {image} to {file_name}. Error: {e}")
+        return False
 
 
 def save_docker_images(images, directory):
@@ -40,9 +53,10 @@ def save_docker_images(images, directory):
         raise DockerDaemonNotRunningError("Docker daemon is not running. Please start Docker and try again.")
 
     for image in images:
-        print(f"Starting to save Docker image {image} as tar archive...", flush=True)
+        print(f"Starting to save Docker image {image} as tar archive...")
         success = save_docker_image(image, directory)
         if success:
             print(f"Successfully saved {image}.\n")
         else:
             print(f"Failed to save {image}.\n")
+
